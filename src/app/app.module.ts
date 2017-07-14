165ffa2d9048ddbe6d9577a9bca9d525f9430012
Felipe Rodríguez <felipe.rodriguez.gimenez@gmail.com>
2017-07-14 13:11:19 +0200

    configuración bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './commom/container/container.component';
+import { HeaderComponent } from './commom/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule
